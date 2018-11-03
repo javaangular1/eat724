94a28b20bf257b003138392681fe15c9f61f4a26
javaangular1 <38805594+javaangular1@users.noreply.github.com>
2018-11-03 23:02:46 +0100

    Conf.js spec file configuration

---
+++ conf.js
@@ -0,0 +1,39 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+var HtmlReporter = require('protractor-beautiful-reporter');
+
+exports.config = {
+
+    directConnect: true,
+
+    capabilities: {
+        browserName: 'chrome'
+    },
+
+    specs: ['main.spec.js'],
+    onPrepare: function () {
+        browser.driver.manage().window().maximize();
+        jasmine.getEnv().addReporter(new SpecReporter({
+            displayFailuresSummary: true,
+            displayFailuredSpec: true,
+            displaySuiteNumber: true,
+            displaySpecDuration: true,
+            showstack: false
+        }));
+        // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+        jasmine.getEnv().addReporter(new HtmlReporter({
+            baseDirectory: '../report/screenshots',
+            preserveDirectory: false,
+            screenshotsSubfolder: 'images',
+            jsonsSubfolder: 'jsons',
+            docName: 'CyberBank-Report.html'
+        }).getJasmine2Reporter());
+
+    },
+
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function () {}
+
+    }
+};
